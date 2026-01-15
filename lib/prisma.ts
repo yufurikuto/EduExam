5487eef9899cc9ce9e98578b05f22da27e956017
yufurikuto <y.ruyu23@gmail.com>
2026-01-15 16:54:49 +0900

    Use fallback string for Prisma datasourceUrl to prevent build crash on missing env

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
     return new PrismaClient({
-        datasourceUrl: process.env.DATABASE_URL,
+        datasourceUrl: process.env.DATABASE_URL || '',
     });
 };
 
